838f5c637a99f2e08658c806d1613beb6d9398ed
Binh2408 <nvbinh24082002@gmail.com>
2025-06-13 06:39:32 +0700

    up

---
+++ src/main/java/org/example/final_module_3/repository/IProductRepository.java
@@ -0,0 +1,15 @@
+package org.example.final_module_3.repository;
+
+
+import java.util.List;
+
+public interface IProductRepository {
+//    List<ProductDTO> findAll();
+//    void add(Product product);
+//    Product findById(String id);
+//    void update(String id, Product product);
+//    void delete(String id);
+//    List<ProductDTO> searchByName(String productName);
+//    List<ProductDTO> searchByNameAndCategory(String productName, String categoryName);
+//    List<ProductDTO> searchByCategory(String categoryName);
+}
